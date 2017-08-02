d53aa4433975633b2e643bef0ba87256727ed5b2
Eric Soderberg <eric.soderberg@hpe.com>
2017-08-02 16:08:45 -0700

    Added theme selector.

--- src/js/components/Doc.js
+++ src/js/components/Doc.js
@@ -1,21 +1,62 @@
 import React, { Component } from 'react';
-
+import PropTypes from 'prop-types';
+import URLSearchParams from 'url-search-params';
+import { hpe } from 'grommet/themes';
 import { Box, Grommet } from 'grommet';
 
 import Props from './Props';
 
+const THEMES = {
+  grommet: undefined,
+  hpe,
+};
+
 export default class Doc extends Component {
+  constructor(props, context) {
+    super(props, context);
+    this.onTheme = this.onTheme.bind(this);
+    this.state = {};
+    const { router: { route: { location } } } = this.context;
+    if (location.search) {
+      const params = new URLSearchParams(location.search);
+      this.state.theme = params.get('theme');
+    }
+  }
+
+  onTheme(theme) {
+    const { router: { history, route: { location } } } = this.context;
+    let loc = location.pathname;
+    if (theme !== 'grommet') {
+      loc += `?theme=${theme}`;
+      this.setState({ theme });
+    } else {
+      this.setState({ theme: undefined });
+    }
+    history.replace(loc);
+  }
+
   render() {
     const { children, component, name } = this.props;
+    const { theme } = this.state;
     return (
-      <Grommet>
+      <Grommet theme={theme ? THEMES[theme] : undefined}>
         <Box direction='row' full='grow'>
           <Box flex='grow' direction='column'>
             {children}
           </Box>
-          <Props name={name} component={component} />
+          <Props
+            name={name}
+            component={component}
+            themes={Object.keys(THEMES)}
+            theme={theme}
+            onTheme={this.onTheme}
+          />
         </Box>
       </Grommet>
     );
   }
 }
+
+Doc.contextTypes = {
+  router: PropTypes.any,
+};
--- src/js/screens/Home.js
+++ src/js/screens/Home.js
@@ -1,45 +1,71 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import URLSearchParams from 'url-search-params';
 
 import { Box, Grid, Grommet, Heading, Paragraph, RoutedButton, TextInput } from 'grommet';
+import { hpe } from 'grommet/themes';
 
-export default () => (
-  <Grommet>
-    <Grid
-      rows={['xsmall', 'flex']}
-      columns={['flex']}
-      areas={[
-        { name: 'header', start: [0, 0], end: [0, 0] },
-        { name: 'main', start: [0, 1], end: [0, 1] },
-      ]}
-    >
-      <Box
-        gridArea='header'
-        direction='row'
-        justify='between'
-        align='center'
-        background='brand'
-        pad={{ horizontal: 'medium' }}
+const THEMES = {
+  grommet: undefined,
+  hpe,
+};
+
+const Home = (props, context) => {
+  const { router: { route: { location } } } = context;
+  let theme;
+  let suffix;
+  if (location.search) {
+    const params = new URLSearchParams(location.search);
+    const themeName = params.get('theme');
+    theme = THEMES[themeName];
+    suffix = `?theme=${themeName}`;
+  }
+  return (
+    <Grommet theme={theme}>
+      <Grid
+        rows={['xsmall', 'flex']}
+        columns={['flex']}
+        areas={[
+          { name: 'header', start: [0, 0], end: [0, 0] },
+          { name: 'main', start: [0, 1], end: [0, 1] },
+        ]}
       >
-        <Heading margin='none'>Grommet 2.0</Heading>
-        <span>
-          {'it\'s'} a <RoutedButton path='/box' label='Box' plain={true} /> in
-          a <RoutedButton path='/grid' label='Grid' plain={true} />
-        </span>
-      </Box>
-      <Box gridArea='main' pad='medium' align='start'>
-        <RoutedButton path='/heading' box={true}>
-          <Heading level={2} margin='none'>Heading</Heading>
-        </RoutedButton>
-        <RoutedButton path='/paragraph' plain={true}>
-          <Paragraph>Paragraph of text.</Paragraph>
-        </RoutedButton>
-        <RoutedButton path='/button' label='Button' />
-        <Box margin={{ vertical: 'medium' }}>
-          <RoutedButton path='/text-input' plain={true}>
-            <TextInput placeholder='TextInput' />
-          </RoutedButton>
+        <Box
+          gridArea='header'
+          direction='row'
+          justify='between'
+          align='center'
+          background='brand'
+          pad={{ horizontal: 'medium' }}
+        >
+          <Heading margin='none'>Grommet 2.0</Heading>
+          <span>
+            {'it\'s'}
+            a <RoutedButton path={`/box${suffix}`} label='Box' plain={true} /> in
+            a <RoutedButton path={`/grid${suffix}`} label='Grid' plain={true} />
+          </span>
         </Box>
-      </Box>
-    </Grid>
-  </Grommet>
-);
+        <Box gridArea='main' pad='medium' align='start'>
+          <RoutedButton path={`/heading${suffix}`} box={true}>
+            <Heading level={2} margin='none'>Heading</Heading>
+          </RoutedButton>
+          <RoutedButton path={`/paragraph${suffix}`} plain={true}>
+            <Paragraph>Paragraph of text.</Paragraph>
+          </RoutedButton>
+          <RoutedButton path={`/button${suffix}`} label='Button' />
+          <Box margin={{ vertical: 'medium' }}>
+            <RoutedButton path={`/text-input${suffix}`} plain={true}>
+              <TextInput placeholder='TextInput' />
+            </RoutedButton>
+          </Box>
+        </Box>
+      </Grid>
+    </Grommet>
+  );
+};
+
+Home.contextTypes = {
+  router: PropTypes.any,
+};
+
+export default Home;
