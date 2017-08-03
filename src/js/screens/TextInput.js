61bfacce805d5044196c3afe15eed0e49fd06d2d
Alan Souza <alansouzati@gmail.com>
2017-08-03 11:52:40 -0700

    Added example for text input suggestions.

--- src/js/screens/TextInput.js
+++ src/js/screens/TextInput.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import { Box, TextInput } from 'grommet';
 import doc from 'grommet/components/text-input/doc';
@@ -7,15 +7,46 @@ import Doc from '../components/Doc';
 
 doc(TextInput);
 
-export default () => (
-  <Doc name='TextInput' component={TextInput}>
-    <Box pad='large'>
-      <Box margin='small'>
-        <TextInput id='item' name='item' />
-      </Box>
-      <Box margin='small'>
-        <TextInput id='item' name='item' placeholder='placeholder' />
-      </Box>
-    </Box>
-  </Doc>
-);
+const allSuggestions = ['alan', 'bryan', 'chris', 'david', 'eric', 'tracy'];
+
+class TextInputDoc extends Component {
+  state = {
+    value: '',
+    suggestions: allSuggestions,
+  }
+  render() {
+    const { suggestions, value } = this.state;
+    return (
+      <Doc name='TextInput' component={TextInput}>
+        <Box pad='large'>
+          <Box margin='small'>
+            <TextInput id='item' name='item' />
+          </Box>
+          <Box margin='small'>
+            <TextInput id='item2' name='item2' placeholder='placeholder' />
+          </Box>
+          <Box margin='small'>
+            <TextInput
+              id='item3'
+              name='item3'
+              placeholder='suggestions'
+              suggestions={suggestions}
+              onSelect={
+                ({ suggestion }) => this.setState({ value: suggestion })
+              }
+              onDOMChange={event => this.setState({
+                value: event.target.value,
+                suggestions: allSuggestions.filter(
+                  suggestion => suggestion.indexOf(event.target.value) > -1
+                ),
+              })}
+              value={value}
+            />
+          </Box>
+        </Box>
+      </Doc>
+    );
+  }
+}
+
+export default TextInputDoc;
