458eacb655415fe3e49b81e869794e1ac2676904
Alan Souza <alansouzati@gmail.com>
2017-09-08 15:09:46 -0700

    Changed the way the theme updates the history (#25)
    * Changed the way the theme updates the history
    
    * Fixed theme managements based on eric's feedback
    
    * updated yarn lock

--- src/js/App.js
+++ src/js/App.js
@@ -1,5 +1,12 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import URLSearchParams from 'url-search-params';
+import { Router, Route, Switch } from 'react-router-dom';
+
+import { createBrowserHistory } from 'history';
+
+import { Grommet } from 'grommet';
+import { hpe } from 'grommet/themes';
 
 import Box from './screens/Box';
 import Button from './screens/Button';
@@ -15,25 +22,86 @@ import Stack from './screens/Stack';
 import Text from './screens/Text';
 import TextInput from './screens/TextInput';
 
-export default class BasicApp extends Component {
+const history = createBrowserHistory();
+
+const THEMES = {
+  grommet: undefined,
+  hpe,
+};
+
+export default class App extends Component {
+  static childContextTypes = {
+    currentTheme: PropTypes.string,
+    onThemeChange: PropTypes.func,
+  };
+
+  state = {
+    theme: undefined,
+  };
+
+  getChildContext() {
+    return {
+      currentTheme: this.state.theme,
+      onThemeChange: (...args) => this.onThemeChange(...args),
+    };
+  }
+
+  componentDidMount() {
+    if (!this.unlisten) {
+      this.unlisten = history.listen((location) => {
+        const { theme } = this.state;
+        if (!location.search && theme && theme !== 'grommet') {
+          // this is to support routes without the theme as a url search param
+          history.replace(`${window.location.pathname}?theme=${theme}`);
+        }
+      });
+    }
+    if (window.location.search) {
+      const params = new URLSearchParams(window.location.search);
+      /* eslint-disable react/no-did-mount-set-state */
+      this.setState({ theme: params.get('theme') });
+      /* eslint-enable react/no-did-mount-set-state */
+    }
+  }
+
+  componentWillUnmount() {
+    if (this.unlisten) {
+      this.unlisten();
+      this.unlisten = undefined;
+    }
+  }
+
+  onThemeChange(theme) {
+    let loc = window.location.pathname;
+    if (theme !== 'grommet') {
+      loc += `?theme=${theme}`;
+      this.setState({ theme }, () => history.replace(loc));
+    } else {
+      this.setState({ theme: undefined }, () => history.replace(loc));
+    }
+  }
+
   render() {
+    const { theme } = this.state;
     return (
-      <Router>
-        <Switch>
-          <Route exact={true} path='/' component={Home} />
-          <Route exact={true} path='/box' component={Box} />
-          <Route exact={true} path='/button' component={Button} />
-          <Route exact={true} path='/chart' component={Chart} />
-          <Route exact={true} path='/heading' component={Heading} />
-          <Route exact={true} path='/grid' component={Grid} />
-          <Route exact={true} path='/layer' component={Layer} />
-          <Route exact={true} path='/markdown' component={Markdown} />
-          <Route exact={true} path='/meter' component={Meter} />
-          <Route exact={true} path='/paragraph' component={Paragraph} />
-          <Route exact={true} path='/stack' component={Stack} />
-          <Route exact={true} path='/text' component={Text} />
-          <Route exact={true} path='/text-input' component={TextInput} />
-        </Switch>
+      <Router history={history}>
+        <Grommet theme={theme ? THEMES[theme] : undefined}>
+          <Switch>
+            <Route exact={true} path='/' component={Home} />
+            <Route exact={true} path='/box' component={Box} />
+            <Route exact={true} path='/button' component={Button} />
+            <Route exact={true} path='/chart' component={Chart} />
+            <Route exact={true} path='/heading' component={Heading} />
+            <Route exact={true} path='/grid' component={Grid} />
+            <Route exact={true} path='/layer' component={Layer} />
+            <Route exact={true} path='/markdown' component={Markdown} />
+            <Route exact={true} path='/meter' component={Meter} />
+            <Route exact={true} path='/paragraph' component={Paragraph} />
+            <Route exact={true} path='/stack' component={Stack} />
+            <Route exact={true} path='/text' component={Text} />
+            <Route exact={true} path='/text-input' component={TextInput} />
+          </Switch>
+        </Grommet>
       </Router>
     );
   }
